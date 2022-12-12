546d73ddfebafc077cc4516f72c26efd5b59f4bc
James Sun <jamessun92@gmail.com>
2022-12-12 00:38:27 -0500

    add comments

--- 2022/Day12.kt
+++ 2022/Day12.kt
@@ -14,14 +14,14 @@ fun main() {
     part2()
 }
 
-private lateinit var input: List<List<Char>>
+private lateinit var grid: List<List<Char>>
 private lateinit var startPos: Pair<Int, Int>
 private lateinit var endPos: Pair<Int, Int>
 
 private fun parseInput() {
-    input = File("input.txt").readLines()
+    grid = File("input.txt").readLines()
         .map { it.toCharArray().toList() }
-    input.forEachInGrid { row, col, cell ->
+    grid.forEachInGrid { row, col, cell ->
         when (cell) {
             'S' -> startPos = row to col
             'E' -> endPos = row to col
@@ -30,14 +30,17 @@ private fun parseInput() {
 }
 
 private fun part1() {
+    // find the shortest path from S to E
+    // 2 nodes are considered adjacent if the dest is at most 1 unit higher than the src
+    // all edges between adjacent nodes have weight 1
     val path = findShortestPath(
         start = startPos,
         end = endPos,
         getAdjacentNodes = { (r, c) ->
-            val current = input[r][c].convert()
+            val current = grid[r][c].convert()
             adjacent4Cells(r, c)
                 .filter { (r, c) ->
-                    val cell = input.getCell(r, c, null)?.convert()
+                    val cell = grid.getCell(r, c, null)?.convert()
                         ?: return@filter false
                     cell - current <= 1
                 }
@@ -48,21 +51,24 @@ private fun part1() {
 }
 
 private fun part2() {
+    // find the shortest path from end node to all other nodes
     val paths = findShortestPaths(
         start = endPos,
         getAdjacentNodes = { (r, c) ->
-            val current = input[r][c].convert()
+            val current = grid[r][c].convert()
             adjacent4Cells(r, c)
                 .filter { (r, c) ->
-                    val cell = input.getCell(r, c, null)?.convert()
+                    val cell = grid.getCell(r, c, null)?.convert()
                         ?: return@filter false
                     cell >= current - 1
                 }
         },
         getEdgeWeight = { _, _ -> 1L }
     )
+    // filter for paths that lead to an 'a' and find the shortest of those
+    // since all edges have weight 1, the cost of the path is equal to its length
     println(
-        paths.filter { input.getCell(it.key).convert() == 'a' }
+        paths.filter { grid.getCell(it.key).convert() == 'a' }
             .minOf { it.value.cost }
     )
 }
